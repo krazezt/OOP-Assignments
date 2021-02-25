fb20cf2ac4d7769e23fe989c45e63055624defba
NISHIDO-LAPTOP\admin <quan5555qt@gmail.com>
2021-02-25 22:44:47 +0700

    Rename "src" folder to "Weak1"

--- src/2.2.6/Bai1/resolve.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class resolve {
-    public void start(double a, double b) {
-        JOptionPane.showMessageDialog(null, "Solution : " + (a==0?(b==0?"Countless solution!":"No solution!"):Double.toString((Double)(-b/a))));
-    }
-}
--- src/2.2.6/Bai3/showResult.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class showResult {
-    public void show(int a, Double x1, Double x2) {
-        JOptionPane.showMessageDialog(null, "Solution : " + (a==0?"No solution!":(a==1?("Double solution : "+x1):"x1 = "+x1+", x2 = "+x2)));
-    }
-}
